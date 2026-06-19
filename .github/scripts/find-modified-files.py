0cd971ad516ff141f1abafafcd32982191049788
callacat <callacat@users.noreply.github.com>
2026-06-19 20:20:02 +0800

    fix: zo4/b.smali find path and find-modified-files false positives

--- .github/scripts/find-modified-files.py
+++ .github/scripts/find-modified-files.py
@@ -1,21 +1,65 @@
-import os
+import os, hashlib
 
 modded = 'modded_smali'
 official = 'official_smali'
 
-count = 0
+# Only consider files containing these keywords
+KEY_PATTERNS = [
+    'checkSign', 'CheckSign', 'check_sign',
+    'NovelCommonParam', 'checkSignEnv', 'checkSignRes',
+    'getPackageInfo', 'PackageManager',
+    'CreatorProxy', 'AppFactory',
+    'Lancet', '@Proxy',
+    'signatures', 'signingInfo',
+    'appComponentFactory',
+    'DeviceRegisterManager',
+]
+
+def grep(path, pat):
+    """Quick check if file contains pattern (only first 8KB)."""
+    try:
+        with open(path) as f:
+            buf = f.read(8192)
+            while buf:
+                if pat in buf:
+                    return True
+                buf = f.read(8192)
+    except:
+        pass
+    return False
+
+def is_interesting(path):
+    if not path.endswith('.smali'):
+        return False
+    for pat in KEY_PATTERNS:
+        if grep(path, pat):
+            return True
+    return False
+
+# Phase 1: scan modded for interesting files (fast, 8KB each)
+interesting = set()
+scanned = 0
 for root, dirs, files in os.walk(modded):
     for f in files:
-        if not f.endswith('.smali'):
-            continue
-        mod_path = os.path.join(root, f)
-        off_path = mod_path.replace('modded_smali', 'official_smali', 1)
-        if not os.path.exists(off_path):
-            continue
-        with open(mod_path) as a, open(off_path) as b:
-            if a.read() != b.read():
-                rel = os.path.relpath(mod_path, modded)
-                print(rel)
-                count += 1
+        scanned += 1
+        path = os.path.join(root, f)
+        if is_interesting(path):
+            rel = os.path.relpath(path, modded)
+            interesting.add(rel)
 
-print('TOTAL_MODIFIED: {}'.format(count))
+print('Scanned {} files, found {} interesting'.format(scanned, len(interesting)))
+
+# Phase 2: byte-for-byte compare interesting files
+true_mods = []
+for rel in sorted(interesting):
+    mod_path = os.path.join(modded, rel)
+    off_path = os.path.join(official, rel)
+    if not os.path.exists(off_path):
+        print('PATCH-ONLY: {}'.format(rel))
+        continue
+    with open(mod_path, 'rb') as a, open(off_path, 'rb') as b:
+        if a.read() != b.read():
+            print('MODIFIED: {}'.format(rel))
+            true_mods.append(rel)
+
+print('\nTOTAL_MODIFIED: {}'.format(len(true_mods)))
